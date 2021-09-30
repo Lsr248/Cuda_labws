55859b621416accba0bda7bd5a2da2e5a4025ddf
Ильсияр Фаттахова <69700906+Lsr248@users.noreply.github.com>
2021-09-30 18:42:43 +0300

    Add files via upload
    calculating pi through the integral

---
+++ kernel.cu
@@ -0,0 +1,61 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <cmath> 
+#include <stdio.h>
+
+#define N  100
+
+
+__global__ void add( double h, double *c) {
+	int t = blockIdx.x;
+	double x = h * t;
+	double x1 = h * (t+1);
+	if (t < N)
+	{
+		c[t]= (sqrtf(1.0 - x*x)+ sqrtf(1 - x1*x1))/2;
+	}
+}
+
+int main()
+{
+	// переменные на CPU
+	double  pi=0;
+	double c[N];
+	double* dev_c;
+	double h = 1 / (double)N;
+
+
+
+	cudaMalloc((void**)&dev_c, N*sizeof(double));
+	
+
+
+	// переменные на GPU
+	//double* dev_pi;
+	//double size = sizeof(double); //размерность
+	// выделяем память на GPU
+	
+	//cudaMalloc((void**)&dev_pi, size);
+	// инициализация переменных
+
+	// копирование информации с CPU на GPU
+	// вызов ядра
+	//add << < 1, 1 >> > ( dev_pi);
+	// копирование результата работы ядра с GPU на CPU
+	add<<<N,1>>>(h, dev_c);
+	cudaMemcpy(c, dev_c , N * sizeof(double), cudaMemcpyDeviceToHost);
+
+	for (int i = 0; i < N; ++i)
+	{
+		pi += c[i];
+	}
+	pi = 4 * pi / N;
+	// вывод информации
+	printf( "%f", pi);
+	// очищение памяти на GPU
+	
+	cudaFree(dev_c);
+
+	return 0;
+}
